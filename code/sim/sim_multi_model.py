83e1557b6296ceebad80f7f0f02c2adc75a15b2e
ANNE E. CLARK <aclark4@steelcurtain.grid.gs.washington.edu>
2016-08-18 15:00:21 -0700

    changed simulations to have only one cer population

--- code/sim/sim_multi_model.py
+++ code/sim/sim_multi_model.py
@@ -13,31 +13,28 @@ outdir = '../../results/sim/'
 N0 = int(sys.argv[3])
 
 num_samples_par = int(sys.argv[4])
-num_samples_cer_1 = int(sys.argv[5])
-num_samples_cer_2 = int(sys.argv[6])
-num_samples_cer = num_samples_cer_1 + num_samples_cer_2
+num_samples_cer = int(sys.argv[5])
 num_samples = num_samples_par + num_samples_cer
 
 # migration parameter is 2 * N0 * m, where mij is fraction of i made
 # up of j each generation; need to figure out how to make migration
 # rates equivalent for different models
-par_cer_migration = 2 * N0 * float(sys.argv[7])
+par_cer_migration = 2 * N0 * float(sys.argv[6])
 
 # in generations
-t_cer = float(sys.argv[8]) / (2 * N0)
-t_par_cer = float(sys.argv[9]) / (2 * N0)
+t_par_cer = float(sys.argv[7]) / (2 * N0)
 
 # 13,500 sites to get about 10% with one recombination event, .3% with
 # more than one (based on poisson(.1), 1 recombination per chromosome
 # of average length 750,000)
-num_sites = float(sys.argv[10])
+num_sites = float(sys.argv[8])
 
 # parameter is recombination rate between adjacent bp per generation
 # should probably be 1/750000 + 6.1 * 10^-6 (where 750000 is average
 # chr size)
-rho = 2 * N0 * float(sys.argv[11]) * (num_sites - 1)
+rho = 2 * N0 * float(sys.argv[9]) * (num_sites - 1)
 
-outcross_rate = float(sys.argv[12])
+outcross_rate = float(sys.argv[10])
 
 rho *= outcross_rate
 
@@ -45,7 +42,7 @@ rho *= outcross_rate
 mu = 1.84 * 10 ** -10
 theta = mu * 2 * num_sites * N0
 
-num_reps = int(sys.argv[13])
+num_reps = int(sys.argv[11])
 
 outfilename = 'sim_out_' + tag + '.txt'
 
@@ -56,41 +53,23 @@ ms_command = \
     '/net/gs/vol1/home/aclark4/software/msdir/ms ' + str(num_samples) + ' ' + str(num_reps) + \
     ' -t ' + str(theta) + \
     ' -r ' + str(rho) + ' ' + str(num_sites) + \
-    ' -I 3 ' + str(num_samples_par) + ' ' + str(num_samples_cer_1) + ' ' + str(num_samples_cer_2)
+    ' -I ' + str(num_samples_par) + ' ' + str(num_samples_cer)
 
-# introgression happens continuously from par to cer1 between the
-# present and t_cer (joining of the two cer lineages)
-if model == 'A':
-    ms_command += \
-        ' -m 2 1 ' + str(par_cer_migration) + \
-        ' -em ' + str(t_cer) + ' 2 1 0'
 
-# introgression happens continuously from par to cer1 between t_cer
-# (joining of the two cer lineages) and t_par_cer (joining of par and
-# cer lineages)
-elif model == 'B':
-    # scale so that there's the same total amount of migration
-    par_cer_migration *= t_cer/float(t_par_cer - t_cer)
-    ms_command += \
-        ' -em ' + str(t_cer) + ' 2 1 ' +  str(par_cer_migration) + \
-        ' -em ' + str(t_par_cer) + ' 2 1 0' # this is probably implied
-
-# introgression happens continuously from par to cer1 between the
+# introgression happens continuously from par to cer between the
 # present and t_par_cer (joining of par and cer lineages)
-elif model == 'C':
-    # scale so that there's the same total amount of migration
-    par_cer_migration *= t_cer/float(t_par_cer)
+if model == 'C':
     ms_command += \
         ' -m 2 1 ' + str(par_cer_migration) + \
         ' -em ' + str(t_par_cer) + ' 2 1 0' # this is probably implied
 
 # introgression happens in one pulse, at some time before t_par_cer
 # specified by the number after D
-elif model[0] == 'D':
+elif model[0] == 'I':
     # time of pulse of migration
     t_mig = float(model[1:])
     # pulse of migration for 1 generation of time
-    par_cer_migration *= t_cer * 2 * N0 # need to scale by 2 N0
+    par_cer_migration *= t_par_cer * 2 * N0 # need to scale by 2 N0
     ms_command += \
         ' -em ' + str(t_mig / (2.0 * N0)) + ' 2 1 ' + str(par_cer_migration) + \
         ' -em ' + str((t_mig + 1) / (2.0 * N0)) + ' 2 1 0'
@@ -98,10 +77,9 @@ elif model[0] == 'D':
 # fail
 else:
     print 'incorrect model selection'
-    sys.exit
+    sys.exit()
 
 ms_command += \
-    ' -ej ' + str(t_cer) + ' 3 2' + \
     ' -ej ' + str(t_par_cer) + ' 2 1' + \
     ' -T > ' + outdir + outfilename
 
