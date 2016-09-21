dd985c3468a91b138f3b9ec69eef4ad17c716c4b
ANNE E. CLARK <aclark4@steelcurtain.grid.gs.washington.edu>
2016-09-20 17:19:03 -0700

    extracted averaging/standard error/bootstrapping into a general purpose file

---
+++ code/misc/mystats.py
@@ -0,0 +1,34 @@
+import math
+import numpy.random
+
+def mean(l):
+    if len(l) == 0:
+        return 'NA'
+    return float(sum(l)) / len(l)
+
+def std_dev(l):
+    if len(l) == 0:
+        return 'NA'
+    if len(l) == 1:
+        return 0
+    m = mean(l)
+    return math.sqrt(sum([(x - m)**2 for x in l]) / (len(l) - 1))
+
+def std_err(l):
+    if len(l) == 0:
+        return 'NA'
+    return std_dev(l) / math.sqrt(len(l))
+
+def bootstrap(l, n = 100, alpha = .05):
+    x = len(l)
+    if x == 0:
+        return 'NA', 'NA'
+    a = []
+    for i in range(n):
+        a.append(mean(numpy.random.choice(l, size = x, replace = True)))
+    a.sort()
+    print len(a), a.count(0)
+    print mean(a)
+    return a[int(alpha * n * .5)], a[int((1 - alpha * .5) * n)]
+
+
