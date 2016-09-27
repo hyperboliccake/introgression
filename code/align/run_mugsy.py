471168087e10b1bc109a04ac0f9c399d4a241c8e
ANNE E. CLARK <aclark4@steelcurtain.grid.gs.washington.edu>
2016-09-27 11:18:17 -0700

    now pulls parameters from global parameter file, and also allows for non-reference paradoxus sequences

--- code/align/run_mugsy.py
+++ code/align/run_mugsy.py
@@ -1,45 +1,49 @@
 import os
+sys.path.insert(0, '..')
+import global_params as gp
 
-drc = '/net/akey/vol2/aclark4/nobackup/100_genomes/genomes/'
-refc = 'S288c_SGD-R64.fa'
+# get all non-reference strains of cerevisiae and paradoxus
+s = []
+for d in gp.dirs_cer + gp.dirs_par:
+    fns = os.listdir(d)
+    # only look at fasta files in the directory
+    fns = filter(lambda x: x.endswith(gp.fasta_suffix), fns)
+    # only look at files containing '_chr' which should be chromosome
+    # sequence files
+    fns = filter(lambda x: '_chr' in x, fns)    
+    num_files = len(fns)
+    if num_files == 0:
+        print 'found no chromosome sequence files in', d, '(perhaps you should check the _chr naming convention?)'
+    fns = list(set([x[:x.find('_chr')] for x in fns]))
+    num_strains = len(fns)
+    assert num_files == num_strains * len(gp.chrms_roman), \
+        'some strains in ' + d + ' have the wrong number of chromosome sequence files'
+    entries = [(x, d) for x in fns]
+    s += entries
 
-drp = '/net/akey/vol2/aclark4/nobackup/100_genomes/paradoxus/strains/CBS432/assembly/'
-refp = 'genome.fa'
+a = []
+if gp.resume_alignment:
+    a = os.listdir(gp.alignments_dir)
 
-dc = '/net/akey/vol2/aclark4/nobackup/100_genomes/genomes_gb/'
-c = os.listdir(dc)
-c = list(set([x[:x.find('_chr')]+'.fa' for x in c]))
+# need to add this on the start of each command because os.system()
+# creates a new shell instance every time
+cmd_string_start = 'export MUGSY_INSTALL=' + gp.mugsy_install_path + '; '
+cmd_string_start += 'export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping; '
+cmd_string_start += 'export PERL5LIB=$MUGSY_INSTALL/perllibs; '
 
-# to run with data provided directly by Dan Skelly, instead of
-# downloaded directly from genbank; note that these sequences don't
-# entirely match the feature coordinates in the genbank files, so you
-# really shouldn't use them if you want to do annotations
-#dc = '/net/akey/vol2/aclark4/nobackup/100_genomes/genomes/'
-#c = ['Sigma1278b.fa', 'SK1.fa', 'yjm1078.fa', 'yjm1083.fa', 'yjm1129.fa', 'yjm1133.fa', 'yjm1190.fa', 'yjm1199.fa', 'yjm1202.fa', 'yjm1208.fa', 'yjm1242.fa', 'yjm1244.fa', 'yjm1248.fa', 'yjm1250.fa', 'yjm1252.fa', 'yjm1273.fa', 'yjm1304.fa', 'yjm1307.fa', 'yjm1311.fa', 'yjm1326.fa', 'yjm1332.fa', 'yjm1336.fa', 'yjm1338.fa', 'yjm1341.fa', 'yjm1342.fa', 'yjm1355.fa', 'yjm1356.fa', 'yjm1381.fa', 'yjm1383.fa', 'yjm1385.fa', 'yjm1386.fa', 'yjm1387.fa', 'yjm1388.fa', 'yjm1389.fa', 'yjm1399.fa', 'yjm1400.fa', 'yjm1401.fa', 'yjm1402.fa', 'yjm1415.fa', 'yjm1417.fa', 'yjm1418.fa', 'yjm1419.fa', 'yjm1433.fa', 'yjm1434.fa', 'yjm1439.fa', 'yjm1443.fa', 'yjm1444.fa', 'yjm1447.fa', 'yjm1450.fa', 'yjm1460.fa', 'yjm1463.fa', 'yjm1477.fa', 'yjm1478.fa', 'yjm1479.fa', 'yjm1526.fa', 'yjm1527.fa', 'yjm1549.fa', 'yjm1573.fa', 'yjm1574.fa', 'yjm1592.fa', 'yjm1615.fa', 'yjm189.fa', 'yjm193.fa', 'yjm195.fa', 'yjm244.fa', 'yjm248.fa', 'yjm270.fa', 'yjm271.fa', 'yjm320.fa', 'yjm326.fa', 'yjm428.fa', 'yjm450.fa', 'yjm451.fa', 'yjm453.fa', 'yjm456.fa', 'yjm470.fa', 'yjm541.fa', 'yjm554.fa', 'yjm555.fa', 'yjm627.fa', 'yjm681.fa', 'yjm682.fa', 'yjm683.fa', 'yjm689.fa', 'yjm693.fa', 'yjm969.fa', 'yjm972.fa', 'yjm975.fa', 'yjm978.fa', 'yjm981.fa', 'yjm984.fa', 'yjm987.fa', 'yjm990.fa', 'yjm993.fa', 'yjm996.fa']
+for strain, d in s:
 
-cmd_string = ''
-
-cmd_string += 'export MUGSY_INSTALL=~/software/mugsy; '
-cmd_string += 'export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping; '
-cmd_string += 'export PERL5LIB=$MUGSY_INSTALL/perllibs; '
-
-chrms_roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XIV']
-
-a = os.listdir('../../alignments/genbank/')
-
-for x in c:
-
-    cmd_string = ''
-    
-    cmd_string += 'export MUGSY_INSTALL=~/software/mugsy; '
-    cmd_string += 'export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping; '
-    cmd_string += 'export PERL5LIB=$MUGSY_INSTALL/perllibs; '
-    
-    for chrm in chrms_roman:
-        if 'S288c_CBS432_' + x[:-3] + '_chr' + chrm + '.maf' not in a:
-            # change directory to ../../alignments/skelly/ if you want
-            # to use other data set
-            cmd_string += '~/software/mugsy/mugsy --directory ../../alignments/genbank/ --prefix S288c_CBS432_' + x[:-3] + '_chr' + chrm + ' ' + drc + refc[:-3] + '/' + refc[:-3] + '_chr' + chrm + '.fa' + ' ' + drp + 'CBS432/CBS432_chr' + chrm + '.fa' + ' ' + dc  + x[:-3] + '_chr' + chrm + '.fa' + '; '
+    cmd_string = cmd_string_start
+        
+    for chrm in gp.chrms_roman:
+        if (gp.cer_ref_strain + '_' + gp.par_ref_strain + '_' + x + '_chr' + chrm + gp.alignment_suffix) not in a:
+            cmd_string += gp.mugsy_instal_path + '/mugsy ' + \
+                '--directory ' + gp.alignments_dir + \
+                '--prefix ' + gp.cer_ref_strain + '_' + par_ref_strain + '_' + x + '_chr' + chrm + ' ' + \
+                gp.cer_ref_dir + '/' + gp.cer_ref_strain + '_chr' + chrm + gp.fasta_suffix + ' ' + \
+                gp.par_ref_dir + '/' + gp.par_ref_strain + '_chr' + chrm + gp.fasta_suffix + ' ' + \
+                d + '/' + strain + '_chr' + chrm + gp.fasta_suffix + '; '
     print x
+
     # commands can only be up to a certain length so break it up this way
     os.system(cmd_string)
