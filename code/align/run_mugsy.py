45f89aedefeeda3f503169f649041467265be963
ANNE E. CLARK <aclark4@grid-head1.grid.gs.washington.edu>
2017-09-05 15:29:26 -0700

    moved some functions to align_helpers file

--- code/align/run_mugsy.py
+++ code/align/run_mugsy.py
@@ -1,29 +1,16 @@
+import sys
 import os
 sys.path.insert(0, '..')
+from align_helpers import *
 import global_params as gp
 
 # get all non-reference strains of cerevisiae and paradoxus
-s = []
-for d in gp.dirs_cer + gp.dirs_par:
-    fns = os.listdir(d)
-    # only look at fasta files in the directory
-    fns = filter(lambda x: x.endswith(gp.fasta_suffix), fns)
-    # only look at files containing '_chr' which should be chromosome
-    # sequence files
-    fns = filter(lambda x: '_chr' in x, fns)    
-    num_files = len(fns)
-    if num_files == 0:
-        print 'found no chromosome sequence files in', d, '(perhaps you should check the _chr naming convention?)'
-    fns = list(set([x[:x.find('_chr')] for x in fns]))
-    num_strains = len(fns)
-    assert num_files == num_strains * len(gp.chrms_roman), \
-        'some strains in ' + d + ' have the wrong number of chromosome sequence files'
-    entries = [(x, d) for x in fns]
-    s += entries
+s = get_strains(flatten(gp.non_ref_dirs.values()))
 
+gp_dir = '../'
 a = []
 if gp.resume_alignment:
-    a = os.listdir(gp.alignments_dir)
+    a = os.listdir(gp_dir + gp.alignments_dir)
 
 # need to add this on the start of each command because os.system()
 # creates a new shell instance every time
@@ -31,19 +18,27 @@ cmd_string_start = 'export MUGSY_INSTALL=' + gp.mugsy_install_path + '; '
 cmd_string_start += 'export PATH=$PATH:$MUGSY_INSTALL:$MUGSY_INSTALL/mapping; '
 cmd_string_start += 'export PERL5LIB=$MUGSY_INSTALL/perllibs; '
 
+ref_prefix = '_'.join(gp.alignment_ref_order) + '_'
+ref_dirs = [gp.ref_dir[ref] for ref in gp.alignment_ref_order]
+
 for strain, d in s:
+    print strain
 
     cmd_string = cmd_string_start
         
-    for chrm in gp.chrms_roman:
-        if (gp.cer_ref_strain + '_' + gp.par_ref_strain + '_' + x + '_chr' + chrm + gp.alignment_suffix) not in a:
-            cmd_string += gp.mugsy_instal_path + '/mugsy ' + \
-                '--directory ' + gp.alignments_dir + \
-                '--prefix ' + gp.cer_ref_strain + '_' + par_ref_strain + '_' + x + '_chr' + chrm + ' ' + \
-                gp.cer_ref_dir + '/' + gp.cer_ref_strain + '_chr' + chrm + gp.fasta_suffix + ' ' + \
-                gp.par_ref_dir + '/' + gp.par_ref_strain + '_chr' + chrm + gp.fasta_suffix + ' ' + \
-                d + '/' + strain + '_chr' + chrm + gp.fasta_suffix + '; '
-    print x
+    for chrm in [gp.chrms[-1]]:
+        align_fn = ref_prefix + strain + '_chr' + chrm + gp.alignment_suffix
+        # if we don't already have an alignment for this strain/chromosome, then make one
+        if align_fn not in a:
+            cmd_string += gp.mugsy_install_path + '/mugsy ' + \
+                '--directory ' + gp_dir + gp.alignments_dir + ' ' + \
+                '--prefix ' + ref_prefix + strain + '_chr' + chrm
+            for ref in gp.alignment_ref_order:
+                cmd_string += ' ' + gp.ref_dir[ref] + '/' + \
+                gp.ref_fn_prefix[ref] + '_chr' + chrm + gp.fasta_suffix
+            cmd_string += ' ' + d + '/' + strain + '_chr' + chrm + gp.fasta_suffix + '; '
+
 
     # commands can only be up to a certain length so break it up this way
+    print cmd_string
     os.system(cmd_string)
