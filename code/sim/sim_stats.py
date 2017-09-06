98934d3c810f95256376a4592e08c3ed4f35b570
ANNE E. CLARK <aclark4@grid-head1.grid.gs.washington.edu>
2017-09-06 14:37:33 -0700

    beginning of code to calculate statistics on simulated sequences to see whether they meet expectations

---
+++ code/sim/sim_stats.py
@@ -0,0 +1,8 @@
+# give some summary stats about simulated sequences, such as:
+# - average number of introgressed tracts per genome
+# - average number of non-introgressed tracts per genome
+# - average sequence identity within and between species
+# - 
+
+
+
