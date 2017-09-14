61903a9418d345c945cdaf7cec6af112677fc783
ANNE E. CLARK <aclark4@grid-head1.grid.gs.washington.edu>
2017-09-13 17:15:43 -0700

    moved function for parsing ms tree

--- code/sim/sim_process.py
+++ code/sim/sim_process.py
@@ -44,6 +44,40 @@ def integer_positions(positions, l, allow_multi_hit = False):
         a.append(new_p)
     return a
 
+def parse_ms_tree_helper(t):
+    if '(' not in t:
+        colon_ind = t.find(':')
+        # index from 0 instead of 1
+        return [int(t[:colon_ind]) - 1, float(t[colon_ind+1:])]
+        #return [int(t[:colon_ind]) - 1, Decimal(t[colon_ind+1:])]
+    left = ''
+    right = ''
+    i = -1
+    if t[1] != '(':
+        comma_ind = t.find(',') 
+        left = t[1:comma_ind]
+        right = t[comma_ind+1:t.rfind(')')]
+    else:
+        open_count = 1
+        i = 2
+        while open_count > 0:
+            if t[i] == '(':
+                open_count += 1
+            elif t[i] == ')':
+                open_count -= 1
+            i += 1
+        i = t[i:].find(',')+i
+        left = t[1:i]
+        right = t[i+1:t.rfind(')')]
+
+    time = float(t[t.rfind(':')+1:])
+    #time = Decimal(t[t.rfind(':')+1:])
+    return [parse_ms_tree_helper(left), parse_ms_tree_helper(right), time]
+
+# converts newick tree string to nested list format (assuming labels from ms output)
+def parse_ms_tree(t):
+    return parse_ms_tree_helper(t[:-1] + ':0')
+
 def read_one_sim(f, num_sites, num_samples):
 
     sim = {}
