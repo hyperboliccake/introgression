a7f87e6bb04930c30b316c39bf004e6acfefbe1c
Anne E. Clark <anneec@della5.princeton.edu>
2017-10-03 14:34:06 -0400

    taking tag and suffixes as arguments instead of full filenames

--- code/sim/compare_introgressed_main.py
+++ code/sim/compare_introgressed_main.py
@@ -1,12 +1,22 @@
 import sys
 import sim_process
+from compare_introgressed import *
+sys.path.append('..')
+import global_params as gp
 
 # takes in two sets of introgression calls on same set of data and
 # compares them (for example, actual and predicted regions, or calls
 # from two different prediction methods)
 
-fn1 = sys.argv[1]
-fn2 = sys.argv[2]
+tag = sys.argv[1]
+suffix1 = sys.argv[2]
+suffix2 = sys.argv[3]
+
+gp_dir = '../'
+fn1 = gp_dir + gp.sim_out_dir + gp.sim_out_prefix + tag + \
+      '_introgressed_' + suffix1 + '.txt'
+fn2 = gp_dir + gp.sim_out_dir + gp.sim_out_prefix + tag + \
+      '_introgressed_' + suffix2 + '.txt'
 
 f1 = open(fn1, 'r')
 f2 = open(fn2, 'r')
@@ -19,7 +29,8 @@ while line1 != '':
     d1, rep1, line1 = sim_process.read_introgression(f1, line1)
     d2, rep2, line2 = sim_process.read_introgression(f2, line2)
     assert rep1 == rep2, str(rep1) + ' ' + str(rep2)
-
+    print 'rep', rep1
+    
     
 
 assert line2 == '', line2
