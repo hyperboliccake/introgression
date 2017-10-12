6f135dbf7eab04f44f25c295c08e1b9eba93b28d
Anne E. Clark <anneec@della5.princeton.edu>
2017-10-12 15:39:44 -0400

    added argument for simulation id to be added to filename so there can be multiple different summary files without overwriting

--- code/sim/aggregate_main.py
+++ code/sim/aggregate_main.py
@@ -9,11 +9,15 @@ import global_params as gp
 
 arg_lines = open(sys.argv[1], 'r').readlines()
 tags = [l.split(' ')[0] for l in arg_lines]
+# summary, introgressed_actual, introgressed_predicted, etc
+output_type  = sys.argv[2] 
+sim_id = sys.argv[3]
 
 gp_dir = '../'
-fns = [gp_dir + gp.sim_out_dir + gp.sim_out_prefix + tag + '_' + sys.argv[2] + '.txt' \
+fns = [gp_dir + gp.sim_out_dir + gp.sim_out_prefix + tag + '_' + output_type + '.txt' \
        for tag in tags]
 
-fn_all = gp_dir + gp.sim_out_dir + gp.sim_out_prefix + 'all_' + sys.argv[2] + '.txt'
+fn_all = gp_dir + gp.sim_out_dir + gp.sim_out_prefix + 'all_' + output_type + \
+         '_' + sim_id + '.txt'
 
 aggregate_summary_files(fns, fn_all, tags)
