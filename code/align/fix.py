5f6fd16d99beac3eb8a81f78a3dd1dfe4e9effc0
hyperboliccake <anneec@della5.princeton.edu>
2018-01-04 16:41:36 -0500

    quick hacks

--- code/align/fix.py
+++ code/align/fix.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-fns = filter(lambda x: 'mafft' in x and '1385' not in x, os.listdir('../../alignments/genbank/'))
+fns = filter(lambda x: 'mafft' in x, os.listdir('../../alignments/genbank/'))
 for fn in fns:
     print fn
     f = open('../../alignments/genbank/' + fn, 'r')
@@ -9,23 +9,10 @@ for fn in fns:
     line = f.readline()
     while line != '':
         if line[0] == '>':
-            if len(line.split(' ')) == 2:
-                s = line.split(' ')[0][1:]
-                c = line.split(' ')[1][:-1]
-                x = ''
-                if s == 'S288c':
-                    x = '/net/akey/vol2/aclark4/nobackup/100_genomes/genomes/S288c_SGD-R64/S288c_SGD-R64_' + c + '.fa'
-                elif s == 'CBS432':
-                    x = '/net/gs/vol1/home/aclark4/projects/introgression/data/CBS432/CBS432_' + c + '.fa'
-                else:
-                    assert s[:3] == 'yjm', s
-                    x = '/net/akey/vol2/aclark4/nobackup/100_genomes/genomes_gb/' + s + '_' + c + '.fa'
-                g.write(line[:-1] + ' ' + x + '\n')
-            else:
-                assert len(line.split(' ')) == 3, line.split(' ')
-                g.write(line)                
-        else:
-            g.write(line)
+            if 'CBS432' in line:
+                line_new = '>CBS432 ' + line.strip().split(' ')[-1] + '\n'
+                line = line_new
+        g.write(line)
         line = f.readline()
     g.close()
     f.close()
