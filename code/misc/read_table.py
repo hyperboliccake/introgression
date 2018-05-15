9dc3085fd6363b381847d9befce3d6db67ca85bb
hyperboliccake <anneec@della5.princeton.edu>
2018-05-15 17:59:05 -0400

    added options for header in file and for which column to key by when reading table rows

--- code/misc/read_table.py
+++ code/misc/read_table.py
@@ -1,21 +1,29 @@
 import gzip
-
-def read_table_rows(fn, sep):
+def read_table_rows(fn, sep, header=True, key_ind=0):
+    # returns dictionary of rows keyed by first item in row
 
     f = None
     if fn.endswith('.gz'):
         f = gzip.open(fn, 'rb')
     else:
         f = open(fn, 'r')
-    labels = f.readline()[:-1].split(sep)
+    labels = None
+    if header:
+        labels = f.readline()[:-1].split(sep)
     regions = [line[:-1].split(sep) for line in f.readlines()]
     d = {}
     for region in regions:
-        d[region[0]] = dict(zip(labels[1:], region[1:]))
+        if header:
+            d[region[key_ind]] = \
+                dict(zip(labels[:key_ind] + labels[key_ind + 1:], \
+                         region[:key_ind] + region[key_ind + 1:]))
+        else:
+            d[region[key_ind]] = region[:key_ind] + region[key_ind + 1:]
     f.close()
     return d, labels
 
 def read_table_columns(fn, sep):
+    # returns dictionary of columns, keyed by labels
 
     f = None
     if fn.endswith('.gz'):
@@ -24,11 +32,10 @@ def read_table_columns(fn, sep):
         f = open(fn, 'r')
     labels = f.readline()[:-1].split(sep)
     d = dict(zip(labels, [[] for l in labels]))
-    line = f.readline()
-    while line != '':
+    lines = f.readlines()
+    f.close()
+    for line in lines:
         line = line[:-1].split(sep)
         for i in range(len(labels)):
             d[labels[i]].append(line[i])
-        line = f.readline()
-    f.close()
     return d, labels
