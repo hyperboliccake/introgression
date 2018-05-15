000e190bb33e8fc2482815f91b73eb63aab77d27
hyperboliccake <anneec@della5.princeton.edu>
2018-05-15 18:16:06 -0400

    running for set of parameters varying by twofold

--- code/sim/ils_rosenberg_main.py
+++ code/sim/ils_rosenberg_main.py
@@ -2,4 +2,20 @@ import ils_rosenberg
 import mpmath as mp
 
 mp.dps = 200
-print "%0.200f" % ils_rosenberg.monophyletic_concordance_2(10, 100, 320000000/8000000)
+for n in [8000000./2, 8000000., 8000000.*2]:
+    for t in [375000000./2, 375000000., 375000000.*2]:
+        print t, n
+        print "%0.50f" % (1 - ils_rosenberg.monophyletic_concordance_2(1, 94, t/n))
+        print "%0.50f" % (1 - ils_rosenberg.monophyletic_concordance_2(2, 94, t/n))
+        print "%0.50f" % (1 - ils_rosenberg.monophyletic_concordance_2(10, 94, t/n))
+        print '***'
+
+"""
+T = 375000000 / 4N
+
+    320000000
+
+theta = mu * 4 * num_sites * N
+
+rho factor = 7.425e-06
+"""
