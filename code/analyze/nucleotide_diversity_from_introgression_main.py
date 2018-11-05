9cea8391b7a6c7ccf9091f59ef6f1d2d931fd0e3
hyperboliccake <anneec@della5.princeton.edu>
2018-11-05 14:52:58 -0500

    estimating the fraction of nucleotide diversity contributed by introgressed regions

---
+++ code/analyze/nucleotide_diversity_from_introgression_main.py
@@ -0,0 +1,165 @@
+import re
+import sys
+import os
+import copy
+import math
+import gzip
+import itertools
+sys.path.insert(0, '..')
+import global_params as gp
+sys.path.insert(0, '../align/')
+import align_helpers
+sys.path.insert(0, '../misc/')
+import overlap
+import read_table
+import read_fasta
+import write_fasta
+import mystats
+
+sys.stdout.flush()
+
+def try_int(s, default=-1):
+    try:
+        i = int(s)
+        return i
+    except:
+        return default
+
+def count_diffs(s, t, skip_char = 'N'):
+    assert len(s) == len(t)
+    num = 0
+    den = 0
+    for i in range(len(s)):
+        if s[i] == skip_char or t[i] == skip_char:
+            continue
+        if s[i] != t[i]:
+            num += 1
+        den += 1
+    return num, den
+
+def referize(strain_seq, ref_ind_to_strain_ind, skip_char = 'N'):
+    s = [skip_char for r in ref_ind_to_strain_ind]
+    for i in range(len(ref_ind_to_strain_ind)):
+        si = ref_ind_to_strain_ind[i]
+        if si == -1:
+            continue
+        if strain_seq[si] in 'atgc':
+            s[i] = strain_seq[si]
+    return s
+
+def mark_introgressed(seq, regions, fill='N'):
+    seqi = copy.deepcopy(seq)
+    for start, end in regions:
+        for i in range(start, end+1):
+            seqi[i] = fill
+    return seqi
+
+tag = 'u3_i.001_tv_l1000_f.01'
+
+regions_by_chrm_and_strain = dict(zip(gp.chrms, [{} for i in range(len(gp.chrms))]))
+fn_regions = gp.analysis_out_dir_absolute + tag + '/' + \
+             'introgressed_blocks_filtered_par_' + tag + '_summary_plus.txt'
+d, labels = read_table.read_table_rows(fn_regions, '\t')
+
+for region in d:
+    chrm = d[region]['chromosome']
+    strain = d[region]['strain']
+    if not regions_by_chrm_and_strain[chrm].has_key(strain):
+        regions_by_chrm_and_strain[chrm][strain] = []
+    regions_by_chrm_and_strain[chrm][strain].append((int(d[region]['start']), \
+                                                     int(d[region]['end'])))
+
+strain_dirs = align_helpers.get_strains(gp.non_ref_dirs[gp.master_ref])
+#strain_dirs = strain_dirs[:3]
+num_strains = len(strain_dirs)
+
+#gp.chrms = ['I', 'II']
+
+total_frac = 0
+total_fracs = dict(zip(gp.chrms, [0 for c in gp.chrms]))
+
+total_frac_nonint = 0
+total_fracs_nonint = dict(zip(gp.chrms, [0 for c in gp.chrms]))
+
+num_comparisons = 0
+
+for i in range(num_strains):
+    strain_i, d_i = strain_dirs[i]
+    strain_i_seqs = {}
+    strain_i_seqs_nonint = {}
+    for chrm in gp.chrms:
+        coord_fn = gp.analysis_out_dir_absolute + 'coordinates/' + \
+                   gp.master_ref + '_to_' + strain_i + \
+                   '_chr' + chrm + '.txt.gz'
+        f_coord = gzip.open(coord_fn, 'rb')
+        ref_ind_to_strain_i_ind = [try_int(line[:-1]) for line in f_coord.readlines()]
+
+        strain_fn = d_i + strain_i + '_chr' + chrm + gp.fasta_suffix
+        print strain_i, chrm
+        strain_i_seqs[chrm] = referize(read_fasta.read_fasta(strain_fn)[1][0].lower(),  ref_ind_to_strain_i_ind)
+        strain_i_seqs_nonint[chrm] = copy.deepcopy(strain_i_seqs[chrm])
+        if regions_by_chrm_and_strain[chrm].has_key(strain_i):
+            strain_i_seqs_nonint[chrm] = mark_introgressed(strain_i_seqs[chrm], regions_by_chrm_and_strain[chrm][strain_i])
+
+    for j in range(i+1, num_strains):
+        strain_j, d_j = strain_dirs[j]
+
+        print strain_i, strain_j
+        num_comparisons += 1
+
+        num = 0
+        den = 0
+        num_nonint = 0
+        den_nonint = 0
+        for chrm in gp.chrms:
+
+            coord_fn = gp.analysis_out_dir_absolute + 'coordinates/' + \
+                       gp.master_ref + '_to_' + strain_j + \
+                       '_chr' + chrm + '.txt.gz'
+            f_coord = gzip.open(coord_fn, 'rb')
+            ref_ind_to_strain_ind = [try_int(line[:-1]) for line in f_coord.readlines()]
+            
+            strain_fn = d_j + strain_j + '_chr' + chrm + gp.fasta_suffix
+            strain_j_seq = referize(read_fasta.read_fasta(strain_fn)[1][0].lower(), ref_ind_to_strain_ind)
+
+            num_chrm, den_chrm = count_diffs(strain_i_seqs[chrm], strain_j_seq)
+            num += num_chrm
+            den += den_chrm
+
+            total_fracs[chrm] += float(num_chrm)/den_chrm
+
+            # excluding introgressed
+            if regions_by_chrm_and_strain[chrm].has_key(strain_j):
+                strain_j_seq = mark_introgressed(strain_j_seq, regions_by_chrm_and_strain[chrm][strain_j])
+
+            num_chrm_nonint, den_chrm_nonint = count_diffs(strain_i_seqs_nonint[chrm], strain_j_seq)
+            num_nonint += num_chrm_nonint
+            den_nonint += den_chrm_nonint
+
+            total_fracs_nonint[chrm] += float(num_chrm_nonint)/den_chrm_nonint
+
+            print num_comparisons, chrm, total_fracs[chrm], \
+                total_fracs_nonint[chrm], \
+                1 - total_fracs_nonint[chrm]/total_fracs[chrm]
+
+        total_frac += float(num)/den
+        total_frac_nonint += float(num_nonint)/den_nonint
+
+        print num_comparisons, total_frac, total_frac_nonint, \
+            1 - total_frac_nonint/total_frac
+        sys.stdout.flush()
+
+nuc_div = total_frac/num_comparisons
+nuc_div_nonint = total_frac_nonint/num_comparisons
+
+print nuc_div
+print nuc_div_nonint
+
+f = open(gp.analysis_out_dir_absolute + tag + '/polymorphism/' + \
+         'nucleotide_diversity.txt', 'w')
+f.write('chromosome\tpi\tpi_nonint\n')
+f.write('all\t' + str(nuc_div) + '\t' + str(nuc_div_nonint) + '\n')
+for chrm in gp.chrms:
+    f.write(chrm + '\t' + str(total_fracs[chrm]/num_comparisons) + '\t' + \
+            str(total_fracs_nonint[chrm]/num_comparisons) + '\n')
+f.close()
