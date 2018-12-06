4363a933627b43d156074e97f5a5c0ede4d2cd68
hyperboliccake <anneec@della5.princeton.edu>
2018-12-05 19:36:27 -0500

    functions for searching efficiently using bisect methods

---
+++ code/misc/binary_search.py
@@ -0,0 +1,8 @@
+import bisect
+
+def present(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bisect.bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        return True
+    return False
