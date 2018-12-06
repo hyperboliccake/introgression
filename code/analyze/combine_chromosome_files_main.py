050d1b3b0cfc2170235b0eb6aeb9035e40d0179f
hyperboliccake <anneec@della5.princeton.edu>
2018-12-05 19:39:32 -0500

    for combinining individual chromosome results files since we are processing chromosomes separately

---
+++ code/analyze/combine_chromosome_files_main.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import gzip
+import predict
+sys.path.insert(0, '..')
+import global_params as gp
+
+args = predict.process_predict_args(sys.argv[2:])
+
+header = open(gp.analysis_out_dir_absolute + args['tag'] + '/' + \
+              'introgressed_blocks_' + args['known_states'][0] + \
+              '_' + args['tag'] + '_chr' + gp.chrms[0] + '_quality.txt', 'r').readline()
+
+for species_from in args['known_states']:
+    fn = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
+         'introgressed_blocks_' + species_from + \
+         '_' + args['tag'] + '_quality.txt'
+    f = open(fn, 'w')
+    f.write(header)
+    for chrm in gp.chrms:
+        fn_chrm = gp.analysis_out_dir_absolute + args['tag'] + '/' + \
+                  'introgressed_blocks_' + species_from + \
+                  '_' + args['tag'] + '_chr' + chrm + '_quality.txt'
+        fc = open(fn_chrm, 'r')
+        fc.readline()
+        for line in fc.readlines():
+            f.write(line)
+    f.close()
+
